6a30c35f8e2f860b9955378cd82a97d2ac977c03
RishiSood1 <rishisood@hotmail.co.uk>
2021-03-10 19:36:03 +0000

    Added logout features, user authentication features all completed

--- app.py
+++ app.py
@@ -19,8 +19,8 @@ mongo = PyMongo(app)
 
 
 @app.route("/")
-@app.route("/get_tasks")
-def get_tasks():
+@app.route("/home")
+def home():
     reviews = mongo.db.reviews.find()
     return render_template("home.html", reviews=reviews)
 
@@ -68,6 +68,13 @@ def login():
             return redirect(url_for("login"))
 
     return render_template("login.html")
+
+
+@app.route("/logout")
+def logout():
+    flash("You have been logged out")
+    session.pop("user")
+    return redirect(url_for("login"))
 
 
 if __name__ == "__main__":
