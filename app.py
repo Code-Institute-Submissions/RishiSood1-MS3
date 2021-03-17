c14a9745ecc1ada4ff8078f163a4f884f6608dac
RishiSood1 <rishisood@hotmail.co.uk>
2021-03-17 19:39:42 +0000

    Added docstings to app.py to explain the code

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 from flask import (
-    Flask, flash, render_template, 
+    Flask, flash, render_template,
     redirect, request, session, url_for)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
@@ -18,6 +18,8 @@ app.secret_key = os.environ.get("SECRET_KEY")
 mongo = PyMongo(app)
 users = mongo.db.users
 
+''' this route loads the homepage along with the reviews'''
+
 
 @app.route("/")
 @app.route("/home")
@@ -26,10 +28,17 @@ def home():
     return render_template("home.html", reviews=reviews)
 
 
+"""gets the data from the database.
+So once the image is clicked, the relevant review appears"""
+
+
 @app.route("/movie_details/<review_id>", methods=["GET", "POST"])
 def movie_details(review_id):
     review = mongo.db.reviews.find_one({"_id": ObjectId(review_id)})
     return render_template("movies.html", review=review)
+
+
+'''This route is linked to the form, allows users to add a new review'''
 
 
 @app.route("/new_reviews", methods=["GET", "POST"])
@@ -54,6 +63,9 @@ def new_reviews():
     return render_template("new_review.html")
 
 
+'''This route is linked to the form, allows users to edit an existing review'''
+
+
 @app.route("/edit_review/<review_id>", methods=["GET", "POST"])
 def edit_review(review_id):
     if request.method == "POST":
@@ -76,6 +88,10 @@ def edit_review(review_id):
     return render_template("edit_review.html", review=review)
 
 
+"""This route is linked to the form.
+Allows users to delete a review they have created"""
+
+
 @app.route("/delete_review/<review_id>")
 def delete_review(review_id):
     mongo.db.reviews.remove({"_id": ObjectId(review_id)})
@@ -83,11 +99,17 @@ def delete_review(review_id):
     return redirect(url_for("home"))
 
 
+'''This route is linked to the search bar, users can search for a movie'''
+
+
 @app.route("/search_movie", methods=["GET", "POST"])
 def search_movie():
     data = request.form.get("data")
     reviews = mongo.db.reviews.find({"$text": {"$search": data}})
     return render_template("home.html", reviews=reviews)
+
+
+'''This following 3 routes are for user authentication'''
 
 
 @app.route("/signup", methods=["GET", "POST"])
